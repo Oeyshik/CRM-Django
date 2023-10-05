d79a9e74c500325da762f94445b54c075c67dedd
Oeyshik Das <oeyshikdas1853@gmail.com>
2023-10-05 12:29:25 +0530

    First Commit

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,7 @@ SECRET_KEY = 'django-insecure-2q3!hfs=p!)7h!*s17w!@!0e_j+^&r!*l053ogcu@k*t^f-1*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.cpp']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,6 +113,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
